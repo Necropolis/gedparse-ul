b23d83746f381a60417549dbd211b67efa980b2c
Chris <lordsauronthegreat@gmail.com>
2011-11-17 09:49:37 -0700

    I can has parser?

--- src/Spouse.cpp
+++ src/Spouse.cpp
@@ -9,22 +9,58 @@
 
 namespace FamilySearch { namespace GEDCOM {
     
-    Name& Spouse::getName() {
-        return this->name;
-    }
-    void Spouse::setName(Name name) {
-        this->name = name;
-    }
+    Spouse::Spouse(): standardisedNames(std::list<StandardisedName>()), name(Name()), Attribute() {}
+    
+    Name& Spouse::getName() { return name; }
+    void Spouse::setName(Name name) { this->name = name; }
+    std::list<StandardisedName>& Spouse::getStandardisedNames() { return standardisedNames; }
     
     std::ostream& operator<< (std::ostream& os, Spouse& spouse) {
         os << "1 SPOU " << spouse.name << "\r\n";
+        std::list<StandardisedName>::iterator iter;
+        for (iter=spouse.standardisedNames.begin(); iter!=spouse.standardisedNames.end(); ++iter)
+            os << "2 " << *iter;
         return os;
     }
     
     std::istream& operator>> (std::istream& is, Spouse& spouse) {
         // stream is now a NAME part
         is >> spouse.name;
-        
+        is.ignore(2, '\n');
+        while (is.good()) {
+            while (is.peek()<'0'||is.peek()>'9') {is.get();} // precondition: at line start
+            char c = is.get();
+            if (c=='0') {
+                is.unget();
+                break;
+            } else if (c=='1') {
+                is.unget();
+                break;
+            } else if (c=='2') {
+                std::string line_type;
+                is >> line_type;
+                
+                if (line_type=="STGN") {
+                    StandardisedName stgn;
+                    is >> stgn;
+                    stgn.setGivenName(true);
+                    spouse.standardisedNames.push_back(stgn);
+                } else if (line_type=="STSN") {
+                    StandardisedName stsn;
+                    is >> stsn;
+                    stsn.setGivenName(false);
+                    spouse.standardisedNames.push_back(stsn);
+                } else {
+                    std::cout << "Unknown line type \"" << line_type << "\"" << std::endl;
+                    inspect_stream(is);
+                    throw new std::exception();
+                }
+            } else {
+                std::cout << "Unknown escalation type!" << std::endl;
+                inspect_stream(is);
+                throw new std::exception();
+            }
+        }
         return is;
     }
     
--- src/Spouse.hpp
+++ src/Spouse.hpp
@@ -5,10 +5,11 @@
 
 // stl
 #include <iostream>
-// boost
-#include <boost/scoped_ptr.hpp>
+#include <list>
 // familysearch
+#include "Attribute.hpp"
 #include "Name.hpp"
+#include "StandardisedName.hpp"
 
 #ifndef __SPOUSE_HPP_
 #define __SPOUSE_HPP_
@@ -21,14 +22,17 @@ namespace FamilySearch { namespace GEDCOM {
      * 2 STSN Sth
      * 2 STGN Giv'en Name
      */
-    class Spouse {
+    class Spouse : public Attribute {
     private:
         Name name;        
+        std::list<StandardisedName> standardisedNames;
         
     public:
+        Spouse();
         
         Name& getName();
         void setName(Name);
+        std::list<StandardisedName>& getStandardisedNames();
         
         friend std::ostream& operator<< (std::ostream&, Spouse&);
         friend std::istream& operator>> (std::istream&, Spouse&);
